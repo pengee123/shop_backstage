4755d72615209a006e1fe043fcb656c43fd6f200
pengee <pengee163@163.com>
2021-06-25 08:23:14 +0800

    模板

--- src/views/Home/childrenPage/helloPage.vue
+++ src/views/Home/childrenPage/helloPage.vue
@@ -1,15 +1,24 @@
 <template>
   <div>
-    hello
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 卡片 -->
+    <el-card class="box-card">
+      <div slot="header" class="clearfix">
+        <span>卡片名称</span>
+    </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'hellopage'
-}
+  name: "hellopage",
+};
 </script>
 
 <style lang="less">
-
 </style>
